f1f19d4c8cb916f2e1d60c22734e06f483b81de6
= <maximuzz@yandex.ru>
2018-07-26 21:39:55 +0300

    Add ViewModelTopRatedMovies

---
+++ ExpressProject.Api/Models/ViewModelTopRatedMovies.cs
@@ -0,0 +1,16 @@
+﻿using ExpressProject.TMDBWrapper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExpressProject.Api.Models
+{
+    public class ViewModelTopRatedMovies
+    {
+        public List<MovieInfo> Movies { get; set; }
+        public int TotalPages { get; set; }
+        public int PageNumber { get; set; }
+        public int TotalTopRatedMovies { get; internal set; }
+    }
+}
