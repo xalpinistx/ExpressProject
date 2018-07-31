d8a79da7b1850ba6cb00c3b3610ba87b51d6c4bd
ArtiomTapunov <Tapunov_AT@tut.by>
2018-07-31 13:41:43 +0300

    Work with images have been added to the back end.

--- ExpressProject.TMDBWrapper/ApiRequest/ApiMovieRequest.cs
+++ ExpressProject.TMDBWrapper/ApiRequest/ApiMovieRequest.cs
@@ -1,6 +1,8 @@
 ﻿using ExpressProject.TMDBWrapper.ApiResponse;
+using ExpressProject.TMDBWrapper.Configuration;
 using ExpressProject.TMDBWrapper.Models;
 using ExpressProject.TMDBWrapper.Shims;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,12 +14,23 @@ namespace ExpressProject.TMDBWrapper.ApiRequest
     internal class ApiMovieRequest : ApiRequestBase, IApiMovieRequest
     {
         //private readonly IApiGenreRequest _genreApi;
+        private readonly IMovieDbSettings _settings;
 
         [ImportingConstructor]
         public ApiMovieRequest(IMovieDbSettings settings /*, IApiGenreRequest genreApi*/)
             : base(settings)
         {
             //_genreApi = genreApi;
+            _settings = settings;
+        }
+
+        protected async Task<ApiConfiguration> GetConfiguration(IMovieDbSettings settings)
+        {
+            var configResponse = await new ApiConfigurationRequest(settings).GetAsync();
+            ApiConfiguration config = new ApiConfiguration();
+            config = JsonConvert.DeserializeObject<ApiConfiguration>(configResponse.Json, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });  
+
+            return config;
         }
 
         public async Task<ApiQueryResponse<Movie>> FindByIdAsync(int movieId, string language = "en")
@@ -31,6 +44,11 @@ namespace ExpressProject.TMDBWrapper.ApiRequest
             string command = $"movie/{movieId}";
 
             ApiQueryResponse<Movie> response = await base.QueryAsync<Movie>(command, param);
+
+            var config = await GetConfiguration(_settings);
+            var baseImageUrl = string.Format("{0}{1}", config.Images.SecureRootUrl, config.Images.Posters.Last());
+
+            response.Item.PosterPath = baseImageUrl + response.Item.PosterPath;
 
             return response;
         }
@@ -47,6 +65,11 @@ namespace ExpressProject.TMDBWrapper.ApiRequest
             const string command = "search/movie";
 
             ApiSearchResponse<MovieInfo> response = await base.SearchAsync<MovieInfo>(command, pageNumber, param);
+
+            var config = await GetConfiguration(_settings);
+            var baseImageUrl = string.Format("{0}{1}", config.Images.SecureRootUrl, config.Images.Posters.Last());
+
+            response.Results.All(movie => { movie.PosterPath = baseImageUrl + movie.PosterPath; return true; });
 
             if (response.Error != null)
             {
@@ -70,6 +93,11 @@ namespace ExpressProject.TMDBWrapper.ApiRequest
 
             ApiQueryResponse<Movie> response = await base.QueryAsync<Movie>(command, param);
 
+            var config = await GetConfiguration(_settings);
+            var baseImageUrl = string.Format("{0}{1}", config.Images.SecureRootUrl, config.Images.Posters.Last());
+
+            response.Item.PosterPath = baseImageUrl + response.Item.PosterPath;
+
             return response;
         }
 
@@ -84,6 +112,11 @@ namespace ExpressProject.TMDBWrapper.ApiRequest
             const string command = "movie/now_playing";
 
             ApiSearchResponse<Movie> response = await base.SearchAsync<Movie>(command, pageNumber, param);
+
+            var config = await GetConfiguration(_settings);
+            var baseImageUrl = string.Format("{0}{1}", config.Images.SecureRootUrl, config.Images.Posters.Last());
+
+            response.Results.All(movie => { movie.PosterPath = baseImageUrl + movie.PosterPath; return true; });
 
             return response;
         }
@@ -100,6 +133,11 @@ namespace ExpressProject.TMDBWrapper.ApiRequest
 
             ApiSearchResponse<Movie> response = await base.SearchAsync<Movie>(command, pageNumber, param);
 
+            var config = await GetConfiguration(_settings);
+            var baseImageUrl = string.Format("{0}{1}", config.Images.SecureRootUrl, config.Images.Posters.Last());
+
+            response.Results.All(movie => { movie.PosterPath = baseImageUrl + movie.PosterPath; return true; });
+
             return response;
         }
 
@@ -113,6 +151,11 @@ namespace ExpressProject.TMDBWrapper.ApiRequest
             const string command = "movie/top_rated";
 
             ApiSearchResponse<MovieInfo> response = await base.SearchAsync<MovieInfo>(command, pageNumber, param);
+
+            var config = await GetConfiguration(_settings);
+            var baseImageUrl = string.Format("{0}{1}", config.Images.SecureRootUrl, config.Images.Posters.Last());
+
+            response.Results.All(movie => { movie.PosterPath = baseImageUrl + movie.PosterPath; return true; });
 
             if (response.Error != null)
             {
@@ -134,6 +177,11 @@ namespace ExpressProject.TMDBWrapper.ApiRequest
             const string command = "movie/popular";
 
             ApiSearchResponse<MovieInfo> response = await base.SearchAsync<MovieInfo>(command, pageNumber, param);
+
+            var config = await GetConfiguration(_settings);
+            var baseImageUrl = string.Format("{0}{1}", config.Images.SecureRootUrl, config.Images.Posters.Last());
+
+            response.Results.All(movie => { movie.PosterPath = baseImageUrl + movie.PosterPath; return true; });
 
             if (response.Error != null)
             {
